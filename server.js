7fed3ea2692b145591d735c25c088dbf3ff1cf1b
ludocid <ludocid@flurred.com>
2015-03-03 10:56:07 +0000

    root

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+app.set('views', __dirname);
+app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'jade');
+app.get('*', function(req, res){
+    res.render('index');
+});
+
+app.listen(process.env.PORT, process.env.IP)
