fe0b381efab8ade5f3957b3cf52f97a3d06838cc
ludocid <ludocid@flurred.com>
2015-03-03 16:58:28 +0000

    root

--- public/app/app.js
+++ public/app/app.js
@@ -1,13 +1,7 @@
 'use strict';
 
-var app = angular.module('app', []);
-app.controller('testCtrl', function($scope) {
-    $scope.jobs = [{
-        title: 'Sales Person',
-        description: 'you will fight dragons'
-    }, {
-        title: 'Accountant',
-        description: 'you will use the keyboard'
-    }];
+var app = angular.module('app', [ngResource]);
+app.controller('testCtrl', function($scope, $resource) {
+    $scope.jobs = $resource.('/jobs').query();
 
 });
