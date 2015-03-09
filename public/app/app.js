61704ca2a0625c944a5775c68146d4f714802787
ludocid@flurred.com <ludocid@flurred.com>
2015-03-09 09:57:10 +0000

    root

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,6 @@
 var app = angular.module('app', ['ngResource']);
 app.controller('testCtrl', function($scope, $resource, jobs) {
     $scope.jobs = $resource('/api/jobs').query();
-
     $scope.submit = function() {
         var newJob = {
             title: $scope.title,
